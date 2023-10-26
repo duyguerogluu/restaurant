37b80e1cdbff9ba64f6f2fb0532b5b10e2907645
Your Name <you@example.com>
2023-10-26 14:47:48 +0300

    ..

---
+++ lib/components/bottom_bar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({super.key});
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Ana Sayfa'),
+    Text('Menü'),
+    Text('Sepet'),
+  ];
+
+  static const List<String> _appBarTitles = [
+    'Ana Sayfa',
+    'Menü',
+    'Sepet',
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_appBarTitles[_selectedIndex]),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:restaurant/components/bottom_bar.dart';
+import 'package:restaurant/functions/navigator_ext.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    final box = GetStorage();
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      if (box.read("token") != null) {
+        pushAndPopBack(const BottomBar());
+      } else {
+        pushAndPopBack(const BottomBar());
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TweenAnimationBuilder<double>(
+          tween: Tween<double>(begin: 16.0, end: 36),
+          duration: Duration(seconds: 2),
+          builder: (BuildContext context, double size, Widget? child) {
+            return Text(
+              'Hoşgeldiniz',
+              style: TextStyle(fontSize: size, color: Colors.orange),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
